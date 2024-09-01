bd2c5bbc7c9eade90cfc6ba8d2389648d2d4d52d
Soham Mahajan <ssoham.mahajan22@gmail.com>
2024-09-01 16:11:27 +0530

    #33 fix for cors issue

---
+++ src/main/java/com/learning/quizapp/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.learning.quizapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Adjust port if needed
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
