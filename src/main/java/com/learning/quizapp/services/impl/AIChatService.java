fd8a86ec46cc69a5d35789291398b2ac2a20bbc5
Soham Mahajan <ssoham.mahajan22@gmail.com>
2024-09-06 20:42:24 +0530

    #48 remove unused API key, changed model to 70b,

--- src/main/java/com/learning/quizapp/services/impl/AIChatService.java
+++ src/main/java/com/learning/quizapp/services/impl/AIChatService.java
@@ -15,18 +15,39 @@ public class AIChatService {
     private final OpenAiChatModel chatModel;
     private final String regex = "\\[(.*?)]";
     private final Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
+    private static final String MODEL_NAME = "llama-3.1-70b-versatile";
+    private static final String MAX_QUESTIONS = "10";
+    private static final String DIFFICULTY_LEVEL = "mixed of easy, medium, hard ";
+    // Prompt creation
+    private static final String PROMPT = "Generate " + MAX_QUESTIONS + " multiple choice questions with a difficulty level of " + DIFFICULTY_LEVEL
+            + " on the topic of %s. Ensure the correct answer is one of the options. "
+            + "The output format should be an array of JSON objects with the following properties: "
+            + "questionTitle, option1, option2, option3, option4, correctAnswer, difficultyLevel, and category. "
+            + "Sample output format:\n"
+            + "[\n"
+            + "    {\n"
+            + "        \"questionTitle\": \"Question Title\",\n"
+            + "        \"option1\": \"Option 1\",\n"
+            + "        \"option2\": \"Option 2\",\n"
+            + "        \"option3\": \"Option 3\",\n"
+            + "        \"option4\": \"Option 4\",\n"
+            + "        \"correctAnswer\": \"Correct answer\",\n"
+            + "        \"difficultyLevel\": \"Difficulty level\",\n"
+            + "        \"category\": \"Category\"\n"
+            + "    }\n"
+            + "]\n";
 
     public AIChatService(OpenAiChatModel chatModel) {
         this.chatModel = chatModel;
     }
 
     public String queryAiService(String userQuery) {
-        String modifiedQuery = getModifiedQuery(userQuery);
+        //String modifiedQuery = getModifiedQuery(userQuery);
         ChatResponse modelResponse = chatModel.call(
                 new Prompt(
-                        modifiedQuery,
+                        String.format(PROMPT, userQuery) ,
                         OpenAiChatOptions.builder()
-                                .withModel("llama3-8b-8192")
+                                .withModel(MODEL_NAME)
                                 .withTemperature(1.0f)
                                 .build()
                 ));
@@ -39,11 +60,4 @@ public class AIChatService {
         return null;
     }
 
-    private static String getModifiedQuery(String userQuery) {
-        String extraInfo = " Generate only 10 questions and Format the response as a valid " +
-                "JSON in below format [{\"questionTitle\": \"What is a constructor?\", \"option1\": \"A member of a class\"," +
-                " \"option2\": \"A loop in Python\", \"option3\": \"A data type\", \"option4\": \"A special method\", " +
-                "\"correctAnswer\": \"A special method\", \"difficultyLevel\": \"Medium\", \"category\": \"java\"}] ";
-        return userQuery + extraInfo;
-    }
 }
